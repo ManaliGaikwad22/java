1660597a6a3b92f9497eff78430ef52d08bc66a3
ManaliGaikwad22 <77033185+ManaliGaikwad22@users.noreply.github.com>
2021-01-20 23:04:43 -0500

    Update addNum.java

--- addNum.java
+++ addNum.java
@@ -1,3 +1,10 @@
+/* Write a program to complete the task given below: 
+
+Ask the user to enter any 2 numbers in between 1-10 and add both of them to another variable call z. 
+
+Use z for adding 30 into it and print the final result by using variable results. */
+
+
 import java.util.*;
 
 public class addNum
