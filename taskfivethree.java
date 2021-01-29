b2a0853579c5e98c829ffdd7070a4016aa0c668c
ManaliGaikwad22 <77033185+ManaliGaikwad22@users.noreply.github.com>
2021-01-28 22:52:47 -0500

    Add files via upload

---
+++ taskfivethree.java
@@ -0,0 +1,11 @@
+public class taskfivethree
+{
+    public static void main(String[] args)
+    {
+        String str = " My name is Manali Gaikwad";
+        str = str.replaceAll(" ", "");
+        System.out.println(str);
+
+    }
+
+}
